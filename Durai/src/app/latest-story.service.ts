9ffda475fb0536d04802f3798d783ad8d26b598d
durai4585 <durai.chandra45@gmail.com>
2018-03-24 10:23:42 -0700

    medium fix

--- Durai/src/app/latest-story.service.ts
+++ Durai/src/app/latest-story.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { forkJoin } from "rxjs/observable/forkJoin";
 
-
 @Injectable()
 export class LatestStoryService {
-
   constructor(private _http: HttpClient) { }
-  
-  getScreenShots(private pageNumber : number):Observable<any[]>{
-  
+   
+  getScreenShots(private page : number):Observable<any[]>{  
   return forkJoin([
   
-   	this._http.get('https://api.dribbble.com/v1/shots?sort=recent&access_token=0b8f0bbf2352be625a2e69211592aed5003b101d7fc01c40d2928e1e3e39bfe1&page='+this.pageNumber),
-    this._http.get('https://api.dribbble.com/v1/shots?access_token=0b8f0bbf2352be625a2e69211592aed5003b101d7fc01c40d2928e1e3e39bfe1&page='+this.pageNumber)
+   	this._http.get('https://api.dribbble.com/v1/shots?sort=recent&access_token=0b8f0bbf2352be625a2e69211592aed5003b101d7fc01c40d2928e1e3e39bfe1&page='+ page),
+    this._http.get('https://api.dribbble.com/v1/shots?access_token=0b8f0bbf2352be625a2e69211592aed5003b101d7fc01c40d2928e1e3e39bfe1&page='+ page),
+    this._http.get('https://www.designernews.co/?format=rss',{
+        headers: new HttpHeaders()
+        .set('Content-Type', 'text/xml') 
+        .append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS') 
+        .append('Access-Control-Allow-Origin', '*')
+        .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method")
+  , responseType:'text'})
+
+    //this._http.get('https://medium.com/feed/topic/digital-design',)
   
   ]);
   
--- Durai/src/app/latest-story/latest-story.component.ts
+++ Durai/src/app/latest-story/latest-story.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { forkJoin } from "rxjs/observable/forkJoin";
+import { parseString } from 'xml2js';
+import { XmlObjects } from 'xml-objects';
 
 import { LatestStoryService } from "../latest-story.service";
 
@@ -16,7 +16,7 @@ import { LatestStoryService } from "../latest-story.service";
 export class LatestStoryComponent implements OnInit {
 
  posts = [];
- pageNumber=1;
+  pageNumber=1;
 
   constructor(private http: HttpClient,private latestStoryService : LatestStoryService) {}
 
@@ -29,6 +29,7 @@ export class LatestStoryComponent implements OnInit {
     this.getData();
   }
 
+    
   getData() {
    console.log(this.pageNumber);
     this.latestStoryService.getScreenShots(this.pageNumber).subscribe(results => {
@@ -53,14 +54,16 @@ export class LatestStoryComponent implements OnInit {
         });
       });
 
-      list.forEach(element => {
-      
-        this.posts.push(element);
-      });
-     
+       console.log(results[2]);
+       // console.log(print());
+             // console.log(this.print(results[2]));
+        console.log(JSON.stringify(parseString(results[2].rss.channel.item)));
+    
       console.log(this.pageNumber);
+        
     });
   }
+    
 
 
 }
