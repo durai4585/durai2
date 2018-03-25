60b647387d0eeda8191988ab068e8f00936eae32
durai4585 <durai.chandra45@gmail.com>
2018-03-24 19:49:15 -0700

    rss

--- Durai/src/app/latest-story/latest-story.component.ts
+++ Durai/src/app/latest-story/latest-story.component.ts
@@ -12,61 +12,69 @@ import { XmlObjects } from 'xml-objects';
 import { LatestStoryService } from "../latest-story.service";
 
 @Component({
-  selector: 'app-latest-story',
-  templateUrl: './latest-story.component.html',
-  styleUrls: ['./latest-story.component.css']
+    selector: 'app-latest-story',
+    templateUrl: './latest-story.component.html',
+    styleUrls: ['./latest-story.component.css']
 })
 export class LatestStoryComponent implements OnInit {
 
- posts = [];
- pageNumber=1;
+    posts = [];
+    pageNumber = 1;
 
-  constructor(private http: HttpClient,private latestStoryService : LatestStoryService) {}
+    constructor(private http: HttpClient, private latestStoryService: LatestStoryService) { }
 
 
-  ngOnInit( ) {
-    this.getData();
-  }
-  onScrollDown() {
-    this.pageNumber += 1;    
-    this.getData();
-  }
+    ngOnInit() {
+        this.getData();
+    }
+    onScrollDown() {
+        this.pageNumber += 1;
+        this.getData();
+    }
 
-  getData() {
-   console.log(this.pageNumber);
-    this.latestStoryService.getScreenShots(this.pageNumber).subscribe(results => {
-      let list = [];  
+    getData() {
+        console.log(this.pageNumber);
+        this.latestStoryService.getScreenShots(this.pageNumber).subscribe(results => {
+            let list = [];
 
-      results[0].forEach(element => {
-       console.log(element); 
-        list.push({
-          "title":element.title,
-          "image":element.images.normal,
-          "url":element.html_url,
-          "views_count":element.views_count
+            results[0].forEach(element => {
+                console.log(element);
+                list.push({
+                    "title": element.title,
+                    "image": element.images.normal,
+                    "url": element.html_url,
+                    "views_count": element.views_count
+                });
+            });
+            results[1].forEach(element => {
+                console.log(element);
+                list.push({
+                    "title": element.title,
+                    "image": element.images.normal,
+                    "url": element.html_url,
+                    "views_count": element.views_count
+                });
+            });
+
+            // console.log(results[2] ? results[2] : 'json_data is null or undefin      
+             parseString(results[2], function(err, result) {
+
+                console.log(JSON.parse(JSON.stringify(result)).rss.channel[0].item); 
+                return  JSON.parse(JSON.stringify(result)).rss.channel[0].item;
+            });
+
+
+         
+
+            //console.log(results[3] ? results[3].length : 'json_data is null or undefined    
+            list.forEach(element => {
+
+                this.posts.push(element);
+            });
+
+            console.log(this.pageNumber);
         });
-      });
-       results[1].forEach(element => {
-         console.log(element); 
-        list.push( {
-         "title":element.title,
-          "image":element.images.normal,
-          "url":element.html_url,
-          "views_count":element.views_count
-        });
-      });
-        
-         console.log(results[2]);
-         //console.log(JSON.stringify(parseString(results[2].rss.channel.item)));
-
-      list.forEach(element => {
-      
-        this.posts.push(element);
-      });
-     
-      console.log(this.pageNumber);
-    });
-  }
+    }
 
 
 }
